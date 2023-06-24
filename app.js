119fb12770b8a899434ba949e2271b98986f9fbd
Umar khan <125694026+Umarkhan0@users.noreply.github.com>
2023-06-25 01:10:50 +0500

    Add buttons

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ let openNav = () =>{
 let closeNav = () =>{
     document.getElementById("mySidenav").style.width = "0";
 }
+// let KfcFoodData = {
+//     burger:{
+//         image: "images/afc4e8b0-ff99-11ed-8640-872ee63b5da0-Krunch-2023-05-31095826.png",
+
+//     }
+// }
